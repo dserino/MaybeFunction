a34a631b01b7a7eff99469fcd7469e2520f92ccd
Dan Serino <serino.daniel@gmail.com>
2016-05-17 00:55:25 -0400

    first commit

---
+++ app/src/main/java/com/erdsrpi/maybefunction/MaybeFunction.java
@@ -0,0 +1,19 @@
+package com.erdsrpi.maybefunction;
+
+/**
+ * Created by dan on 5/17/16.
+ */
+
+public class MaybeFunction {
+
+
+    public MaybeFunction() {
+
+    }
+
+    public String generateAnswer(String Question) {
+
+        return "Ben and Paulina are destined to be together.";
+    }
+
+}
